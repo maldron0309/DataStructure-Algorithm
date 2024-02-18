b386ed90e3617cfba160491ceba0387fb0f1301e
maldron0309 <bagjinhyeong640@gmail.com>
2024-02-19 00:14:53 +0900

    Stack

--- 1.DataStructure/Stack/Stack/Stack.cpp
+++ 1.DataStructure/Stack/Stack/Stack.cpp
@@ -1,12 +1,66 @@
-﻿
+﻿#include <iostream>
+#include <vector>
 
-#include <iostream>
-#include <iostream>
 using namespace std;
 
 
+class Stack
+{
+	int arr[100];
+	int index = -1;
+public:
+
+	int top()
+	{
+		if (empty())
+		{
+
+			throw runtime_error("Stack is empty");
+		}
+		else
+		{
+			return arr[index];
+		}
+	}
+
+	void pop()
+	{
+		if (!empty())
+		{
+			--index;
+		}
+		return;
+	}
+
+	void push(int val)
+	{
+		if (index == 99)
+		{
+			cout << "FULL" << "\n";
+		}
+		else
+		{
+			arr[++index] = val;
+		}
+	}
+
+	bool empty()
+	{
+		return index == -1;
+	}
+
+};
+
 int main()
 {
+	Stack S;
 
+	for (int i = 0; i < 5; i++) {
+		S.push(i);
+	}
+
+	for (; !S.empty(); S.pop()) {
+
+		cout << S.top() << "\n";
+	}
 }
-
